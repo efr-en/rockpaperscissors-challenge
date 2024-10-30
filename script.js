355c8a411c9f7e4db4b2e0de0980856fd6f9e081
efr-en <efrenlaris@gmail.com>
2024-10-29 21:57:36 -0400

    fixed case code by adding result = to display results

--- script.js
+++ script.js
@@ -12,8 +12,20 @@ function playGame(playerChoice) {
     } else {
         switch(playerChoice) {
             case 'rock':
-                (computerChoice === 'scissors') ? 'You win!' : 'Computer wins!';
+                result = (computerChoice === 'scissors') ? 'You win!' : 'Computer wins!';
+                break;
+
+            case 'paper':
+                result = (computerChoice === 'rock') ? 'You win!' : 'Computer wins!';
+                break;
+
+            case 'scissors':
+                result = (computerChoice === 'paper') ? 'You win!' : 'Computer wins!';
                 break;
         }
     }
+    playerDisplay.textContent = `Player: ${playerChoice}`;
+    computerDisplay.textContent = `Computer: ${computerChoice}`;
+    resultDisplay.textContent = result;
+
 }
